8e061d8f87e9497849cdfb5304b753058af62107
Roro <mckenizerochelle@yahoo.com>
2017-10-29 21:53:56 -0500

    sub 4

--- fifteen.js
+++ fifteen.js
@@ -1,206 +1,139 @@
 $(document).ready(function(){
     
     
-            //Global Variables
+    var $pa = $('#puzzlearea').children();
     
-    //Store divs as variables
-    var $pieces = $('#puzzlearea').children();
     
-    //Create variables to manage coordinates
-    var row = 0;
-    var topCoord = 0;
-    var leftCoord = 0;
-    
-    //Create variables to manage background position
-    var xPos = 0;
-    var yPos = 0;
-    
-    //Initialize empty square
-    var emptyX = 0;
-    var emptyY = 0;
+    var topp = 0;
+    var leftt = 0;
+    var rows = 0;
    
-    //Counter for shuffle function
-    var counter = 0;
+    var x = 0;
+    var y = 0;
     
-    
-            //Helper Functions
-    
-    //Calculate background position
-    function BackPos(pos1,pos2){
+    var emp1 = 0;
+    var emp2= 0;
+   
+    var count = 0;
+
+    function BP(pos1,pos2){
         return ('' + pos1 + 'px' + ' ' + pos2 + 'px')
     }
     
-    
-    
-    
-    //Set the puzzle in order
-    function setPuzzle(){
-        //Add class to divs to create puzzle pieces
-        $pieces.addClass('puzzlepiece');
+    function order(){
+        $pa.addClass('puzzlepiece');
         
+        $pa.each(function(){
+            $(this).css({top: topp,left: leftt,backgroundPosition: BP(x,y) });
 
-        //align pieces
-        $pieces.each(function(){
-            $(this).css({top: topCoord,left: leftCoord,backgroundPosition: BackPos(xPos,yPos) });
-
-            //Skip if statements for each row until conditions are true
-            //Hence if statements will be triggered from the bottom up to allow leftCoord to be reset and used before being incremented in the new line
-
-             //Row Four
-            if((row > 14) && (row <= 17))
+            if((rows > 14) && (rows <= 17))
             {
-                leftCoord += 100;
-                row++;
-                
-                //Move along image
-                xPos -= 100;
-                
+                leftt += 100;
+                rows++;
+                x-= 100;  
             }
             
-            if(row == 18)
+            if(rows == 18)
                 {
-                  emptyX = leftCoord;
-                  emptyY = topCoord;
+                  emp1 = leftt;
+                  emp2 = topp;
                 }
 
-            //Row Three
-            if((row > 9) && (row <= 13))
+            if((rows > 9) && (rows <= 13))
             {
-                leftCoord += 100;
-                row++;
-                
-                //Move along image
-                xPos -= 100;
+                leftt += 100;
+                rows++;
+
+                x -= 100;
             }
 
-             if(row == 14)
+             if(rows == 14)
             {
-                //Move to next row
-                topCoord += 100;
-                row++;
-
-
-                //Set vertical image position
-                yPos = 100;
-
-                //Reset left coordinate
-                leftCoord = 0;
-                
-                //Reset horizontal position for image
-                xPos = 0;
+                topp += 100;
+                rows++;
+                y = 100;
+                leftt = 0;
+                x = 0;
             }
 
 
-            //Row Two
-            if((row > 4) && (row <= 8))
+            if((rows > 4) && (rows <= 8))
             {
-                leftCoord += 100;
-                row++;
-                
-                //Move along image
-                xPos -= 100;
+                leftt += 100;
+                rows++;
+                x -= 100;
             }
 
 
-            if(row == 9)
+            if(rows == 9)
             {
-                //Move to next row
-                topCoord += 100;
-                row++;
-                
-                //Set vertical image position
-                yPos = 200;
-
-                //Reset left coordinate
-                leftCoord = 0;
-                
-                //Reset horizontal position for image
-                xPos = 0;
+                topp += 100;
+                rows++;
+                y = 200;
+                leftt = 0;
+                x = 0;
             }
 
-
-            //Row One
-            if(row <= 3){
-                leftCoord += 100;
-                row++;
-                
-                //Move along image
-                xPos -= 100;
+            if(rows <= 3){
+                leftt += 100;
+                rows++;
+                x -= 100;
             }
 
-            //Move to next row
-            if(row == 4)
+            if(rows == 4)
             {
-                topCoord += 100;
-                row++;
-                
-                //Set vertical image position
-                yPos = 300;
-
-                //Reset left coordinate
-                leftCoord = 0;
-                
-                //Reset horizontal position for image
-                xPos = 0;
+                topp += 100;
+                rows++;
+                y = 300;
+                leftt = 0;
+                x = 0;
                 
             }
 
 
         });
     }
+
+    function canmove(obj){
     
-    
-    //Determine whether or not a function is movable
-    function isMovable(obj){
-        
-        //Empty space is in row above or below the square
-         if(obj.position().top == (emptyX + 100)  || obj.position().top == (emptyX - 100))
+         if(obj.position().top == (emp1 + 100)  || obj.position().top == (emp1 - 100))
          {
-             //Space is in same row therefore directly above or below
-             if(obj.position().left == emptyY) {
+             if(obj.position().left == emp2) {
                 return true;
                  
              }
          }
         
-        //Empty space is in row beside the square
-         if(obj.position().left == (emptyY + 100)  || obj.position().left == (emptyY - 100))
+         if(obj.position().left == (emp2 + 100)  || obj.position().left == (emp2 - 100))
          {
-             //Space is in the same column therefore directly beside the square
-             if(obj.position().top == emptyX) {
+             if(obj.position().top == emp1) {
                 return true;
                  
              }
          }
     }
     
-     //Indicate a movable piece with a color change
     function indicateMovable(){
         
-       if(isMovable($(this))){
+       if(canmove($(this))){
              $(this).addClass('movablepiece');
           }
     }
-    
-    //Move pieces
+
     function movePiece()
     {
-        
-          //Temporary variables for switching
         var TempX = 0;
         var TempY = 0;
-    
-        //Position of the variable to be moved
+
         var moveTop = $(this).position().top;
         var moveLeft = $(this).position().left;
         
-        if(isMovable($(this))){
-            //Perform switch
-            TempX = emptyX;
-            TempY = emptyY;
+        if(canmove($(this))){
+            TempX = emp1;
+            TempY = emp2;
 
-            emptyX = moveTop;
-            emptyY = moveLeft;
+            emp1 = moveTop;
+            emp2 = moveLeft;
 
             moveTop = TempX;
             moveLeft = TempY;
@@ -208,32 +141,23 @@ $(document).ready(function(){
 
             $(this).css({top: moveTop,left: moveLeft }); 
         }
-    }
-    
-    //Random 
+    } 
     function shuffle(){
-        var randNum = Math.floor(Math.random() * 15);
-        
-         //Randomly choose object
-        var obj = $pieces[randNum];
-        
-         //Temporary variables for switching
+        var rand = Math.floor(Math.random() * 15);
+
+        var obj = $pa[rand];
         var TempX = 0;
         var TempY = 0;
-        
-       
-       
-        //Position of the variable to be moved
+
         var moveTop = $(obj).position().top;
         var moveLeft = $(obj).position().left;
         
-        if(isMovable($(obj))){
-            //Perform switch
-            TempX = emptyX;
-            TempY = emptyY;
+        if(canmove($(obj))){
+            TempX = emp1;
+            TempY = emp2;
 
-            emptyX = moveTop;
-            emptyY = moveLeft;
+            emp1 = moveTop;
+            emp2 = moveLeft;
 
             moveTop = TempX;
             moveLeft = TempY;
@@ -242,17 +166,14 @@ $(document).ready(function(){
             $(obj).css({top: moveTop,left: moveLeft });
             
             
-            counter += 1;
-            
-            //Recursively call function until puzzle is shuffled
-            if(counter !== 100){
+            count += 1;
+            if(count !== 100){
                 shuffle();
             }
             else{
-                counter = 0;
+                count = 0;
             }
         }
-        //Recursively call function movable item is selected
         else{
             shuffle();
         }
@@ -261,64 +182,32 @@ $(document).ready(function(){
     
     
    
-    function RandomBG(){
-        
-        //Generate random number
-        var randNum = Math.floor(Math.random() * 8);
-        
-        //Store puzzle board
+    function RBg(){
+        var rand = Math.floor(Math.random() * 8);
         var $board = $('#puzzlearea');
     
-        switch(randNum)
+        switch(rand)
         {
             case 0:
-                  $pieces.css({'background-image':'url(https://github.com/CodeKingClarke/info2180-project2/blob/gh-pages/background.jpg?raw=true)'});
+                  $pa.css({'background-image':'url(https://github.com/RoroMcky/info2180-project2/blob/master/inuyasha.jpg)'});
                 break;
             case 1:
-                 $pieces.css({'background-image':'url(https://github.com/CodeKingClarke/info2180-project2/blob/gh-pages/Barcelona.jpg?raw=true)'});
-                break;
-            case 2:
-                 $pieces.css({'background-image':'url(https://github.com/CodeKingClarke/info2180-project2/blob/gh-pages/Eiffel%20Tower.jpg?raw=true)'});
-                break;
-            case 3:
-                 $pieces.css({'background-image':'url(https://github.com/CodeKingClarke/info2180-project2/blob/gh-pages/Paris%20Walk.jpg?raw=true)'});
-                 
-                break;
-            case 4:
-                 $pieces.css({'background-image':'url(https://github.com/CodeKingClarke/info2180-project2/blob/gh-pages/Spider%20Wallpaper%202.jpg?raw=true)'});
-                break;
-            case 5:
-                 $pieces.css({'background-image':'url(https://github.com/CodeKingClarke/info2180-project2/blob/gh-pages/Tokyo%20Ghoul.jpg?raw=true)'});
-                break;
-            case 6:
-                 $pieces.css({'background-image':'url(https://github.com/CodeKingClarke/info2180-project2/blob/gh-pages/Valencia.jpg?raw=true)'});
-                break;
-            case 7:
-                 $pieces.css({'background-image':'url(https://github.com/CodeKingClarke/info2180-project2/blob/gh-pages/Drawing%20Hands.jpg?raw=true)'});
+                 $pa.css({'background-image':'url(https://github.com/RoroMcky/info2180-project2/blob/master/background.jpg)'});
                 break;
             default:
-                $pieces.css({'background-image':'url(https://github.com/CodeKingClarke/info2180-project2/blob/gh-pages/Paris%20Walk.jpg?raw=true)'});    
+                $pa.css({'background-image':'url(https://github.com/RoroMcky/info2180-project2/blob/master/lelouch.jpg)'});    
         }
     }
+
+    order();
     
- 
-    
-            // Main Game
-    
-    
-    //Begin Game
-    setPuzzle();
-    
-    //Choose random background
-    RandomBG();
-     
-    //Indicate a movable piece
-    $pieces.hover(indicateMovable);
    
-    //Move pieces
-    $pieces.click(movePiece);
+    RBg();
      
-    //Shuffle the board on clicking the shuffle button
+    $pa.hover(indicateMovable);
+   
+    $pa.click(movePiece);
+
     $('#shufflebutton').click(shuffle);
   
     
